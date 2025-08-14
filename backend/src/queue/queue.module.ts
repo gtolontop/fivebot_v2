89274674e52b607e99e64378355b4ae0cd664850
gtolontop <cruckstore+github@gmail.com>
2025-08-15 00:42:29 +0200

    feat: Add Prisma and Encryption modules to QueueModule for enhanced functionality

--- backend/src/queue/queue.module.ts
+++ backend/src/queue/queue.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SimpleQueueService } from './simple-queue.service';
 import { QueueService } from './queue.service';
+import { PrismaModule } from '../common/prisma/prisma.module';
+import { EncryptionModule } from '../common/encryption/encryption.module';
 
 @Module({
+  imports: [PrismaModule, EncryptionModule],
   providers: [
     {
       provide: QueueService,
