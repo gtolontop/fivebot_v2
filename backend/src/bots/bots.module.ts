007aaa154fd191b6e3d0e90f68c460ce5408c85b
gtolontop <cruckstore+github@gmail.com>
2025-08-15 16:06:40 +0200

    feat: Add SetupMetricsService for managing bot metrics and integrate into BotsModule

--- backend/src/bots/bots.module.ts
+++ backend/src/bots/bots.module.ts
@@ -3,6 +3,7 @@ import { BotsService } from './bots.service';
 import { BotsController } from './bots.controller';
 import { BotRecoveryService } from './bot-recovery.service';
 import { BotMetricsService } from './bot-metrics.service';
+import { SetupMetricsService } from './setup-metrics.service';
 import { EncryptionService } from '../common/encryption/encryption.service';
 import { DiscordModule } from '../common/discord/discord.module';
 import { QueueModule } from '../queue/queue.module';
@@ -11,7 +12,7 @@ import { UsersModule } from '../users/users.module';
 @Module({
   imports: [QueueModule, UsersModule, DiscordModule],
   controllers: [BotsController],
-  providers: [BotsService, BotRecoveryService, BotMetricsService, EncryptionService],
+  providers: [BotsService, BotRecoveryService, BotMetricsService, SetupMetricsService, EncryptionService],
   exports: [BotsService, BotRecoveryService],
 })
 export class BotsModule {}
