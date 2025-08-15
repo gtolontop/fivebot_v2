78ed00be8dfa38525c35a89d112694571b9ee096
gtolontop <cruckstore+github@gmail.com>
2025-08-15 16:10:29 +0200

    feat: Add DiscordModule import to CacheModule for enhanced functionality

--- backend/src/common/cache/cache.module.ts
+++ backend/src/common/cache/cache.module.ts
@@ -2,9 +2,11 @@ import { Module, Global } from '@nestjs/common';
 import { CacheService } from './cache.service';
 import { CacheCleanupService } from './cache-cleanup.service';
 import { CacheController } from './cache.controller';
+import { DiscordModule } from '../discord/discord.module';
 
 @Global()
 @Module({
+  imports: [DiscordModule],
   controllers: [CacheController],
   providers: [CacheService, CacheCleanupService],
   exports: [CacheService],
