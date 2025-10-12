73d2bea1aba7c5ff49424f7a9a89e4ae5a12d248
Cruck Store <cruckstore+github@gmail.com>
2025-10-12 15:09:03 +0200

    feat: add CollaboratorsController and service to BotsModule

--- backend/src/bots/bots.module.ts
+++ backend/src/bots/bots.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { BotsService } from './bots.service';
 import { BotsController } from './bots.controller';
 import { TicketMessagesController } from './ticket-messages.controller';
+import { CollaboratorsController } from './collaborators.controller';
 import { BotRecoveryService } from './bot-recovery.service';
 import { BotMetricsService } from './bot-metrics.service';
 import { SetupMetricsService } from './setup-metrics.service';
@@ -10,6 +11,7 @@ import { BotLogsService } from './bot-logs.service';
 import { ConsoleBufferService } from './console-buffer.service';
 import { BotRealtimeMetricsService } from './bot-realtime-metrics.service';
 import { TicketService } from './ticket.service';
+import { CollaboratorsService } from './collaborators.service';
 import { EncryptionService } from '../common/encryption/encryption.service';
 import { DiscordModule } from '../common/discord/discord.module';
 import { QueueModule } from '../queue/queue.module';
@@ -17,8 +19,8 @@ import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [QueueModule, UsersModule, DiscordModule],
-  controllers: [BotsController, TicketMessagesController],
-  providers: [BotsService, BotRecoveryService, BotMetricsService, SetupMetricsService, BotMonitorService, BotLogsService, ConsoleBufferService, BotRealtimeMetricsService, TicketService, EncryptionService],
-  exports: [BotsService, BotRecoveryService, BotLogsService, ConsoleBufferService, BotMetricsService],
+  controllers: [BotsController, TicketMessagesController, CollaboratorsController],
+  providers: [BotsService, BotRecoveryService, BotMetricsService, SetupMetricsService, BotMonitorService, BotLogsService, ConsoleBufferService, BotRealtimeMetricsService, TicketService, CollaboratorsService, EncryptionService],
+  exports: [BotsService, BotRecoveryService, BotLogsService, ConsoleBufferService, BotMetricsService, CollaboratorsService],
 })
 export class BotsModule {}
