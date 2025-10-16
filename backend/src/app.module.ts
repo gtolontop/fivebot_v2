f904be8950f09c1a3e455c76eed152803ebf0ad4
Cruck Store <cruckstore+github@gmail.com>
2025-10-17 00:28:22 +0200

    feat: import EventsModule in app.module.ts for event management

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,6 +14,7 @@ import { BotsModule } from './bots/bots.module';
 import { CreditsModule } from './credits/credits.module';
 import { QueueModule } from './queue/queue.module';
 import { TasksModule } from './tasks/tasks.module';
+import { EventsModule } from './events/events.module';
 import { UrlMetadataController } from './utils/url-metadata.controller';
 import { UploadController } from './utils/upload.controller';
 import { NotificationsModule } from './notifications/notifications.module';
@@ -38,6 +39,7 @@ import { NotificationsModule } from './notifications/notifications.module';
     CreditsModule,
     QueueModule,
     TasksModule,
+    EventsModule,
     NotificationsModule,
   ],
   controllers: [UrlMetadataController, UploadController],
