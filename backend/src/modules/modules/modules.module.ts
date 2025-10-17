0e9270b0f9f239aec4bef3635c2de4dd037614f3
Cruck Store <cruckstore+github@gmail.com>
2025-10-17 11:45:14 +0200

    feat: add ModulesModule to the main application module for module management integration

--- backend/src/modules/modules/modules.module.ts
+++ backend/src/modules/modules/modules.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ModulesController } from './modules.controller';
 import { ModulesService } from './modules.service';
-import { PrismaModule } from '../prisma/prisma.module';
+import { PrismaModule } from '../../common/prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
