4505d4a3dbd9c4b72939236e23b330e6a019756e
Cruck Store <cruckstore+github@gmail.com>
2025-10-18 08:35:13 +0200

    feat: add js-cookie import for cookie management in browse page

--- frontend/src/app/browse/[slug]/page.tsx
+++ frontend/src/app/browse/[slug]/page.tsx
@@ -7,6 +7,7 @@ import { DashboardLayout } from '@/components/layout/DashboardLayout';
 import { designTokens } from '@/styles/design-tokens';
 import axios from 'axios';
 import toast from 'react-hot-toast';
+import Cookies from 'js-cookie';
 
 interface Module {
   id: string;
