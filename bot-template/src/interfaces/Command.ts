04aef03a53fe45da7f3b0930eedccf387726b909
Cruck Store <cruckstore+github@gmail.com>
2025-11-16 01:45:15 +0100

    remove unused file

--- bot-template/src/interfaces/Command.ts
+++
@@ -1,6 +0,0 @@
-import { SlashCommandBuilder, CommandInteraction, ChatInputCommandInteraction } from 'discord.js';
-
-export interface Command {
-  data: SlashCommandBuilder | Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">;
-  execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
-}
