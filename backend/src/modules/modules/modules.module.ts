eb9b35c53a6673fd99a90b5f39647edfc4c6fcf4
Cruck Store <cruckstore+github@gmail.com>
2025-12-20 15:33:33 +0100

    fix: Register ModuleSeederService in ModulesModule to enable auto-seeding

--- backend/src/modules/modules/modules.module.ts
+++ backend/src/modules/modules/modules.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ModulesController } from './modules.controller';
 import { ModulesService } from './modules.service';
+import { ModuleSeederService } from './module-seeder.service';
 import { PrismaModule } from '../../common/prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   controllers: [ModulesController],
-  providers: [ModulesService],
-  exports: [ModulesService],
+  providers: [ModulesService, ModuleSeederService],
+  exports: [ModulesService, ModuleSeederService],
 })
 export class ModulesModule {}
